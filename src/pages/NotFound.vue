b579732f668a892ef39aa1f041d0c317873384ad
gio <giorgimakasarashvili@gmail.com>
2023-04-02 18:53:35 +0400

    fixed routing

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,21 +1,19 @@
 <template>
   <img src="../assets/logo.png" alt="logo" />
-  <h2>404</h2>
-  <h3>Page Not Found</h3>
+  <h2>oops...</h2>
+  <h3>
+    Looks like you lost, let me take you at <router-link to="/home">home</router-link>
+  </h3>
 </template>
 
-<script>
-export default {};
-</script>
-
 <style scoped>
-*{
-    text-align: center;
-    color: white;
+* {
+  text-align: center;
+  color: white;
 }
-img{
-    position: relative;
-    left: 50%;
-    transform: translateX(-50%);
+img {
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
